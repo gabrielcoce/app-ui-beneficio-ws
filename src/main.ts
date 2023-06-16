0860e67853adc6d4fcfcec14d9daf37b1cb7f09c
Gabriel Coc Estrada <gabrielcoce@outlook.com>
2023-06-15 18:08:09 -0600

    feat: se quitan logs en pro y se agregan libs a angular.json

--- src/main.ts
+++ src/main.ts
@@ -1,7 +1,15 @@
 import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 
 import { AppModule } from './app/app.module';
+import { enableProdMode } from '@angular/core';
+import { environment } from './environments/environment';
 
-
+if (environment.production) {
+  enableProdMode();
+  /*
+    con esta línea de código a continuacion eliminamos los console.log en producción
+  */
+  window.console.log = () => {};
+}
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
